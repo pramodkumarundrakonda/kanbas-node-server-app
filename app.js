4f7e882cc2a6aa273d400375c9e8f9a4f9af262d
pramodkumarundrakonda <pramodkumarundrakonda@gmail.com>
2023-12-04 22:58:08 -0500

    completed a6

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
-import express from "express";
-import Hello from "./hello.js";
-import Lab5 from "./Lab5.js";
-import cors from "cors";
-import CourseRoutes from "./courses/routes.js"
-import ModuleRoutes from "./modules/routes.js";
+import express from 'express';
 import "dotenv/config";
-
+import session from "express-session";
+import Hello from "./hello.js";
+import Lab5 from './lab5.js';
+import CourseRoutes from "./courses/routes.js";
+import cors from "cors";
+import ModuleRoutes from './modules/routes.js';
+import AssignmentRoutes from './assignments/routes.js';
+import mongoose from 'mongoose';
+import UserRoutes from './users/routes.js';
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
 const app = express();
-app.use(express.json());
-app.use(cors()
+app.listen(process.env.PORT || 4000);
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+    })
 );
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
+app.use(express.json());
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
-Lab5(app)
-Hello(app)
-app.listen(process.env.PORT || 4000);
+AssignmentRoutes(app);
+Lab5(app);
+Hello(app);
